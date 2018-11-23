fe28ec357b017ff961b81e1c552f8bbfd1d5308c
unknown <olg@mgroup.com>
2018-11-23 17:14:17 +0800

    使用vant list列表加载，滚动到一定距离再次加载

--- config/index.js
+++ config/index.js
@@ -10,7 +10,18 @@ module.exports = {
     // Paths
     assetsSubDirectory: 'static',
     assetsPublicPath: '/',
-    proxyTable: {},
+    proxyTable: {
+
+    },
+
+  //   proxyTable: {
+  //     '/api': {
+  //         target: 'http://localhost:3000',
+  //         pathRewrite:{
+  //             '^/api':'/'
+  //         }
+  //     },
+  // },
 
     // Various Dev Server settings
     host: 'localhost', // can be overwritten by process.env.HOST
--- src/views/components/order-list-item/index.vue
+++ src/views/components/order-list-item/index.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
-      <ul class="lists">
+      <!-- <ul class="lists">
           <li class="lists-item" v-for="(item,index) in data" :key="index"> 
             <router-link class="lists-div" tag="div" :to="{path:'/details'}">
                <div class="imgs">
@@ -17,7 +17,32 @@
                </div>
             </router-link>
           </li>
+      </ul> -->
+      <van-list
+        v-model="loading"
+        :finished="finished"
+        @load="onLoad"
+        >
+         <ul class="lists">
+          <li class="lists-item" v-for="(item,index) in list" :key="index"> 
+            <router-link class="lists-div" tag="div" :to="{path:'/details'}">
+               <div class="imgs">
+                   <img :src="item.imgSrc" alt="">
+               </div>
+               <div class="info">
+                   <h6 class="label">{{item.name}}</h6>
+                   <p class="desc">服务时间:{{item.time}}</p>
+                   <p class="desc">服务时长:{{item.duration}}</p>
+                   <p class="desc">服务地点:{{item.address}}</p>
+                   <p class="price">￥
+                       <span class="total">{{item.price}}</span>
+                   </p>
+               </div>
+            </router-link>
+          </li>
       </ul>
+        
+    </van-list>
          
     </div>
 </template>
@@ -26,10 +51,32 @@
 export default {
     data(){
         return{
-       
+          list: [],
+      loading: false,
+      finished: false
         }
     },
-    props:['data']
+    props:['data'],
+    mounted(){
+        // this.list=this.data;
+    },
+    methods:{
+        onLoad() {
+      // 异步更新数据
+      setTimeout(() => {
+        for (let i = 0; i < 2; i++) {
+          this.list.push(this.data[i]);
+        }
+        // 加载状态结束
+        this.loading = false;
+
+        // 数据全部加载完成
+        if (this.list.length >= this.data.length) {
+          this.finished = true;
+        }
+      }, 1000);
+    }
+    }
 
 }
 </script>
